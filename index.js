42b83b87a6fd494e191f8b6bd4d167237b2dffe4
Julie <47457976+GiuliaGiuliaBel@users.noreply.github.com>
2020-04-07 20:31:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,48 @@
 
 let start = document.getElementById('start');
 
-let depositCheck = document.querySelector('.deposit-check');
-
-let buttons = document.getElementsByTagName('button');
-
-// Работаем с HTML коллекцией
-// HTMLCollection, хранящая элементы DOM, является динамической. При изменении документа она моментально отражает все произведённые изменения.
-let btnPlusIncomeAdd = buttons.item(0);
-let btnPlusExpensesAdd = buttons.item(1);
-
-// resultTotal 
-let budgetMonthValue = document.getElementsByClassName('result-total budget_month-value');
-let budgetDayValue = document.getElementsByClassName('result-total budget_day-value');
-let expensesMonthValue = document.getElementsByClassName('result-total expenses_month-value');
-let additionalIncomeValue = document.getElementsByClassName('result-total additional_income-value');
-let additionalExpensesValue = document.getElementsByClassName('result-total additional_expenses-value');
-let incomePeriodValue = document.getElementsByClassName('result-total income_period-value');
-let targetMonthValue = document.getElementsByClassName('result-total target_month-value');
+let depositCheck = document.querySelector('#deposit-check');
+console.log(depositCheck);
 
 // Работаем с NodeList
 // Метод querySelectorAll() Document возвращает статический (не динамический) NodeList, содержащий все найденные элементы документа, которые соответствуют указанному селектору.
-let additional_incomeItem = document.querySelectorAll('.additional_income-item')[0];
-let additional_incomeItem2 = document.querySelectorAll('.additional_income-item')[1];
+let additional_income = document.querySelectorAll('.additional_income-item');
+let additional_incomeItem = additional_income[0];
+
+console.log(additional_incomeItem);
+
+let additional_incomeItem2 = additional_income[1];
+console.log(additional_incomeItem2);
+
+let buttons = document.getElementsByTagName('button');
+
+let btnPlusIncomeAdd = buttons[0];
+console.log(btnPlusIncomeAdd);
+
+let btnPlusExpensesAdd = buttons[1];
+console.log(btnPlusExpensesAdd);
+
+// resultTotal 
+let budgetMonthValue = document.getElementsByClassName('result-total budget_month-value')[0];
+let budgetDayValue = document.getElementsByClassName('result-total budget_day-value')[0];
+let expensesMonthValue = document.getElementsByClassName('result-total expenses_month-value')[0];
+let additionalIncomeValue = document.getElementsByClassName('result-total additional_income-value')[0];
+let additionalExpensesValue = document.getElementsByClassName('result-total additional_expenses-value')[0];
+let incomePeriodValue = document.getElementsByClassName('result-total income_period-value')[0];
+let targetMonthValue = document.getElementsByClassName('result-total target_month-value')[0];
+
+console.log(budgetMonthValue, budgetDayValue, expensesMonthValue, additionalIncomeValue, additionalExpensesValue, incomePeriodValue, targetMonthValue);
 
 // получаем оставшиеся Input со страницы
-let salaryAmount = document.querySelector('salary-amount');
-let incomeTitle = document.querySelector('income-title');
-let incomeAmount = document.querySelector('income-amount');
-let expensesTitle = document.querySelector('expenses-title');
-let expensesAmount = document.querySelector('expenses-amount');
-let additionalExpensesItem = document.querySelector('additional_expenses-item');
-let depositAmount = document.querySelector('deposit-amount');
-let depositPercent = document.querySelector('deposit-percent');
-let targetAmount = document.querySelector('target-amount');
-let periodSelect = document.querySelector('period-select');
+let salaryAmount = document.querySelector('.salary-amount');
+let incomeTitle = document.querySelector('.income-title');
+let incomeAmount = document.querySelector('.income-amount');
+let expensesTitle = document.querySelector('.expenses-title');
+let expensesAmount = document.querySelector('.expenses-amount');
+let additionalExpensesItem = document.querySelector('.additional_expenses-item');
+let depositAmount = document.querySelector('.deposit-amount');
+let depositPercent = document.querySelector('.deposit-percent');
+let targetAmount = document.querySelector('.target-amount');
+let periodSelect = document.querySelector('.period-select');
 
+console.log(salaryAmount, incomeTitle , incomeAmount, expensesTitle, expensesAmount, additionalExpensesItem, depositAmount, depositPercent, targetAmount, periodSelect);
