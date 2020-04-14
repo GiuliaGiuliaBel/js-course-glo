510eb82c0cbe82463a7512a2cf39084e234dfcbf
Julie <47457976+GiuliaGiuliaBel@users.noreply.github.com>
2020-04-14 20:11:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ const DomElement = function(selector, height, width, bg, fontSize) {
     }
     
     DomElement.prototype.createElem = function() {
-            if(this.selector[0] === '.'){
-                let divWithClass = document.createElement('div');
-                divWithClass.style.backgroundColor = this.bg;
-                divWithClass.style.height = this.height + 'px';
-                divWithClass.style.width = this.width + 'px';
-                divWithClass.style.fontSize = this.fontSize;
-                divWithClass.classList.add('new-class');
-                divWithClass.textContent = 'Привет, мир!';
-                let newElem = document.getElementById('root');
-                newElem.append(divWithClass)
-                      
-        } else if (this.selector[0] === '#'){
-                let divWithId = document.createElement('div');
-                divWithId.style.backgroundColor = this.bg;
-                divWithId.style.height = this.height + 'px';
-                divWithId.style.width = this.width + 'px';
-                divWithId.style.fontSize = this.fontSize;
-                divWithId.setAttribute('id', 'best');
-                divWithId.textContent = 'Привет, мир!';      
-      };
-    }
+                if(this.selector[0] === '.'){
+                    let divWithClass = document.createElement('div');
+                    divWithClass.style.backgroundColor = this.bg;
+                    divWithClass.style.height = this.height + 'px';
+                    divWithClass.style.width = this.width + 'px';
+                    divWithClass.style.fontSize = this.fontSize;
+                    divWithClass.style.position = 'absolute';
+                    divWithClass.classList.add('new-class');
+                    divWithClass.textContent = 'Привет, мир!';
+                    document.body.append(divWithClass);                        
+            } else if (this.selector[0] === '#'){
+                    let divWithId = document.createElement('div');
+                    divWithId.style.backgroundColor = this.bg;
+                    divWithId.style.height = this.height + 'px';
+                    divWithId.style.width = this.width + 'px';
+                    divWithId.style.fontSize = this.fontSize;
+                    divWithId.style.position = 'absolute';
+                    divWithId.setAttribute('id', 'best');
+                    divWithId.textContent = 'Привет, мир!';
+                    document.body.append(divWithId);          
+          };
+        }      
     
 const domElem = new DomElement('.sdf', '50', '50', 'lightblue', '18');
 domElem.createElem();
