5aa52768ed78c61bb1968614f879d43aa9c01623
Julie <47457976+GiuliaGiuliaBel@users.noreply.github.com>
2020-04-22 19:22:23 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
     function prependZero(timerValue){
         if(timerValue <= 9){
             return '0' + timerValue;
-        }return timerValue
+        }return timerValue;
     }
 
     function updateClock(){
@@ -47,6 +47,92 @@ window.addEventListener('DOMContentLoaded', () => {
    updateClock();
 
 }  
- countTimer('1 May 2000')
+ countTimer('1 May 2000');
+
+ // меню
+const toggleMenu = () => {
+    
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = document.querySelectorAll('ul>li');
+
+    const handlerMenu = () => {
+        menu.classList.toggle('active-menu');
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);  
+
+    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+
+};
+
+toggleMenu();
+
+//popup
+
+//написать анимацию появления модального окна
+const animateModal = () => {
+  
+        const popupContent = document.querySelectorAll('.popup-content');
+
+    popupContent.forEach(elem => {
+        let start = Date.now();
+        let timer = setInterval(() => {
+        let timePassed = Date.now() - start;
+
+        if(timePassed >= 2000){
+            clearInterval(timer);
+            return;
+        }
+
+        draw(timePassed);
+    }, 5);
+
+    function draw(timePassed) {
+        elem.style.left = timePassed / 4 + 'px';
+        }
+    }); 
+};
+
+window.addEventListener('resize', () => {
+    const popup = document.querySelector('.popup'),
+    popupContent = document.querySelector('.popup-content'),
+      
+            if(document.documentElement.clientWidth > 768){
+             
+                popup.style.display = 'block';
+                    animateModal();                                            
+                }else {
+                    popupContent.removeAttribute('style')
+                    popup.style.display = 'block';
+                }  
+});
+  
+const togglePopup = () => {
+     const popup = document.querySelector('.popup'),
+     popupContent = document.querySelectorAll('.popup-content'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close');
+        
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                if(document.documentElement.clientWidth > 768){
+                    popup.style.display = 'block';
+                    animateModal();
+                    }
+                    popup.style.display = 'block';                           
+                    popupContent.style = 0;
+            });
+       }); 
+
+        popupClose.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });  
+};
+
+togglePopup();
 });        
+    
     
