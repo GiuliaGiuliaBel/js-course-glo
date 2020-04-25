369161e7cf04e0bce34b3119e2ee8f45ad122fb9
Julie <47457976+GiuliaGiuliaBel@users.noreply.github.com>
2020-04-25 20:19:33 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
+  
     'use strict';
-
+    
     function countTimer(deadline) {
         //получить элементы со страницы
         let timerHours = document.querySelector('#timer-hours'),
@@ -46,28 +47,107 @@ window.addEventListener('DOMContentLoaded', () => {
   
    updateClock();
 
-}  
- countTimer('1 May 2020');
-    
- //меню
- 
- const toggleMenu = () => {
-    const btnMenu = document.querySelector('.menu'),
-        menu = document.querySelector('menu'),
-        closeBtn = document.querySelector('.close-btn'),
-        //получить вложенные элементы в меню
-        menuItems = menu.querySelectorAll('ul>li');
+};
 
-    const handlerMenu = () => {
-        menu.classList.toggle('active-menu');
-    };
+countTimer('1 May 2020');
 
-    btnMenu.addEventListener('click', handlerMenu);
-    closeBtn.addEventListener('click', handlerMenu);
-    menuItems.forEach(elem => elem.addEventListener('click', handlerMenu));
+const toggleMenu = () => {
+    const menu = document.querySelector('menu');
+
+    document.body.addEventListener('click', (e) => {
+        let target = e.target;
+        
+        if(target.closest('.menu')){
+            menu.classList.add('active-menu')
+        }
+        else {
+            menu.classList.remove('active-menu')
+        }           
+    });
+       
 };
  
  toggleMenu();
+
+ let count = 0;
+ let idInter;
+ let animatePopup = function animate() {
+   
+    idInter = requestAnimationFrame(animatePopup);
+     const popup = document.querySelector('.popup'),
+     popupContent = popup.querySelector('.popup-content');
+   
+     count++;    
+      
+         if((popupContent.style.left.slice(0, popupContent.style.left.length-2)) < (popup.clientWidth/2.5)){        
+             popupContent.style.left = count*15 + 'px';
+         }else{
+             cancelAnimationFrame(idInter);
+             count = 0;
+         }
+ }; 
+
+ // табы
+
+const tabs = () => {
+    const tabHeader = document.querySelector('.service-header'),
+        tab = tabHeader.querySelectorAll('.service-header-tab'),
+        tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+            for(let i = 0; i < tabContent.length; i++){
+                if(index === i){
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        };
+
+        tabHeader.addEventListener('click', e => {
+            let target = e.target;
+            while(target !== tabHeader){
+                if(target.classList.contains('service-header-tab')){
+                    tab.forEach((item, i) => {
+                         if(item === target){
+                            toggleTabContent(i);                  
+                         }
+                    });  
+                    return;          
+                 }
+                 target = target.parentNode;
+            }         
+        });
+};
+tabs();
+
+
+ //popup
+ const togglePopup = () => {
+    const popup = document.querySelector('.popup'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupContent = popup.querySelector('.popup-content'),
+        popupClose = document.querySelector('.popup-close');
+      
+        popupBtn.forEach(elem => {
+          
+            elem.addEventListener('click', () => {
+                if(document.documentElement.clientWidth > 768){
+                    popup.style.display = 'block';
+                    idInter = requestAnimationFrame(animatePopup);
+                }  
+                popup.style.display = 'block';
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+            popupContent.removeAttribute('style');
+        });
+ };
+ togglePopup();
 
  const slider = () => {
      
@@ -160,9 +240,11 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         });
 
-        startSlide(1000);
+        startSlide(3000);
  };
 
  slider();
+
 });        
+    
 
