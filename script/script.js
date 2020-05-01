44de5b5b358678b3fc4735ecb1c6d454c8fa6c40
Julie <47457976+GiuliaGiuliaBel@users.noreply.github.com>
2020-05-01 19:35:00 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -407,21 +407,11 @@ tabs();
         //если что-то меняется на инпутах, селектах
     calcBlock.addEventListener('change', e => {
         const target = e.target;
-        //три варианта записи условия if
-        // if(target.matches('.calc-type') || target.matches('.calc-square') ||
-        //     target.matches('.calc-day') || target.matches('.calc-count')){
-        //         console.log(target);              
-        //     }
-        // if(target === calcType || target === calcSquare ||
-        //     target === calcDay || target === calcCount){
-        //         console.log(1);               
-        //     }
+       
         if(target.matches('select') || target.matches('input')){
             countSum();    
-        }
-        
+        }        
     });
-
   };
 
   calc(100);
@@ -478,56 +468,56 @@ tabs();
         form.addEventListener('submit', (event) => {
             event.preventDefault();
          
-                 preloader.classList.add('loaded');
-                 form.appendChild(preloader);
+            preloader.classList.add('loaded');
+            form.appendChild(preloader);
             const formData = new FormData(form);
                  
-            let body = {};           
-        
+            let body = {};    
+            
             formData.forEach((val, key) => {
                 body[key] = val;
             });
-            postData(body, 
-                () => {
-                    preloader.classList.remove('loaded');
-                    preloader.style.color = 'white';
-                    preloader.textContent = successMessage;
-                
-            },
-             (error) => {
-                preloader.textContent = errorMessage;
-                console.log(error);               
-            });
-        });
+        
+            const outputData = () => {
+                preloader.classList.remove('loaded');
+                preloader.textContent = successMessage;
+                document.getElementById(formId).reset();
+            };
 
-        // outputdata - callback -функция, обработка данных, которые вернулись
-        const postData = (body, outputData, errorData) => {
-            const request = new XMLHttpRequest();
-            request.addEventListener('readystatechange', () => {
-
-                if(request.readyState !== 4) {
-                    return;
-                }
-
-                if(request.status === 200) {
-                    outputData();
-                  
-                } else {
-                    errorData(request.status);
-                   
-                }
-            });
-            request.open('POST', './server.php');
-            request.setRequestHeader('Content-Type', 'application-json');
-            //получить данные с формы
+        const postData = (body) => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.addEventListener('readystatechange', () => {
     
-            //отправить даныне на сервер
-            request.send(JSON.stringify(body));
-            //очистить форму после отправки
-            document.getElementById(formId).reset();
-           
+                    if(request.readyState !== 4) {
+                        return;
+                    }
+    
+                    if(request.status === 200) {
+                        resolve();
+                      
+                    } else {
+                        reject(request.statusText);
+                       
+                    }
+                });
+                request.open('POST', './server.php');
+                request.setRequestHeader('Content-Type', 'application-json');
+        
+                request.send(JSON.stringify(body));          
+               
+            });     
         }
+
+        postData(body)
+            .then(outputData)
+            .catch(error => {
+                preloader.classList.remove('loaded');
+                preloader.textContent = errorMessage;
+            });
+        });   
   };
+
 
 sendForm('form1');
 sendForm('form2');
